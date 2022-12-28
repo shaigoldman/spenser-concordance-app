1321343b3754ef5779579751b71f2ff023632c96
Shai Goldman <shaitzvi@gmail.com>
2022-12-27 19:30:51 -0500

    say which entires shown

--- src/components/EntriesPage.tsx
+++ src/components/EntriesPage.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import './EntriesPage.css';
 import { Entry } from './Entry'
 import { ConcordanceI } from '../Interfaces/Concordance';
+import { H4 } from '@blueprintjs/core';
 
 interface EntriesPageProps {
   concordance: ConcordanceI
   start: number
   size: number
 }
-export const EntriesPage = ({concordance, start, size}: EntriesPageProps) =>
-  <div className="Entry-List">
-        {concordance.slice(start, start+size).map(
+export const EntriesPage = ({concordance, start, size}: EntriesPageProps) => {
+  
+  const slice = concordance.slice(start, start+size)
+
+  return (
+    <>
+      <div className='Entry-List-Title'>
+        <H4>
+          Displaying entries {start}-{start+size} ("
+          {slice[0].word}" to 
+          "{slice[slice.length-1].word}"):
+        </H4>
+      </div>
+      <div className="Entry-List">
+        {slice.map(
           (e, i) => <Entry entry={e} key={"entry"+i}/>)}
-  </div>
+      </div>
+    </>
+  )
+}
