d754ea7cc7178e147396814fedb401cbc3d740eb
Shai Goldman <shaitzvi@gmail.com>
2022-12-29 17:04:27 -0500

    src to link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
 import './Header.css'
 import { H1, H4 } from "@blueprintjs/core"
-import spenserImg from '../resources/spenser.jpeg'
 
 export const Header = () => <div className="Title">
   <H1>Spenser Concordance</H1>
   <H4>An Online Concordance For the Works</H4>
   <H4>Of Edmund Spenser</H4>
-  <img src={spenserImg} alt="Edmund Spenser" className="Image"/>
+  <img src="https://raw.githubusercontent.com/shaigoldman/spenser-concordance-app/main/src/resources/spenser.jpeg" alt="Edmund Spenser" className="Image"/>
   <div className='header-space'></div>
 </div>
